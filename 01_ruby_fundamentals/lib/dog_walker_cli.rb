e09963ffa24205b7b96553ce20cf67751fa53b2d
Shelby Talbert <shelby.talbert@flatironschool.com>
2022-06-02 21:01:00 -0400

    lesson one complete

--- 01_ruby_fundamentals/lib/dog_walker_cli.rb
+++ 01_ruby_fundamentals/lib/dog_walker_cli.rb
@@ -1,5 +1,5 @@
 def start_cli
-  puts "Hi there! Welcome to the Dog Walker CLI!".cyan
+  puts "Hi there! Welcome to the Dog Walker CLI!".blue
 end
 
 # define a method `print_menu_options` which outlines the user's
@@ -8,7 +8,12 @@ end
 #   2. Add a dog
 # At any time, type "menu" to see these options again or "exit" to
 # leave the program
-
+def print_menu_options
+  puts "Here are your options:
+  1. List all dogs
+  2. Add a dog"
+  puts "Please choose an option. At any time, type 'exit' to leave the program"
+end
 
 
 
@@ -16,6 +21,14 @@ end
 # if the user types "exit" we'll print a message thanking them
 # for using the CLI and invoke exit to terminate the program
 # otherwise, return whatever the user typed in
+def ask_for_choice
+  input = gets.chomp
+  if input == "exit"
+    puts "Thanks for using the Dog Walker CLI - buh bye!"
+    exit
+  end
+  input
+end
 
 
 
@@ -30,12 +43,26 @@ Lennon Snow
 
 =end
 
+def print_dog dog_hash
+  puts
+  puts dog_hash[:name].yellow
+  puts "  Age: #{dog_hash[:age]}"
+  puts "  Breed: #{dog_hash[:breed]}"
+  puts "  Image Url: #{dog_hash[:image_url]}"
+  puts
+end
+
 
 
 
 # define a method `list_dogs` that will iterate over an array of
 # dog hashes and call print_dog on each one.
-
+def list_dogs(dog_arr)
+  dog_arr.each do |dog_hash|
+    print_dog(dog_hash)
+  end
+  "you're welcome"
+end
 
 
 # define an `add_dog` method which accepts an array of dog
@@ -45,12 +72,42 @@ Lennon Snow
 # Take this information and put it into a hash
 # add the hash to the dogs array passed as an argument
 # print the newly added dog
+def add_dog all_dogs
+  print "please enter your dog's name "
+  name = ask_for_choice
+
+  print "please enter your dog's age "
+  age = ask_for_choice
+
+  print "please enter your dog's breed "
+  breed = ask_for_choice
+
+  print "please enter your dog's image url "
+  image_url = ask_for_choice
+  
+  new_dog = {name: name, age: age, breed: breed, image_url: image_url}
+  all_dogs.push new_dog
+  # all_dogs << {:name => name, :age => age, :breed => breed, :image_url => image_url}
+  puts "Your dog has been added. Thanks for playing."
+  print_dog new_dog
+end
 
 
 
 # define a method `handle_choice` which will take a `choice` as a 
 # parameter and handle it in the appropriate way based on the menu
 # option that was chosen
+def handle_choice(choice)
+  if choice == "1"
+    list_dogs($dogs)
+  elsif choice == "2"
+    add_dog($dogs)
+  elsif choice == "debug"
+    binding.pry
+  else 
+    puts "oops you let the dogs out, grrrr."
+  end
+end
 
 
 
